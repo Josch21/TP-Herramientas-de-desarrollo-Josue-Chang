1b7d91641b98f7d272cb5b722918d5e829e1ebf9
Josch21 <139594929+Josch21@users.noreply.github.com>
2023-09-22 18:25:03 -0500

    Trabajo

---
+++ Assets/Scripts/Mechanics/PlayerController.cs
@@ -0,0 +1,186 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Platformer.Gameplay;
+using static Platformer.Core.Simulation;
+using Platformer.Model;
+using Platformer.Core;
+
+namespace Platformer.Mechanics
+{
+    /// <summary>
+    /// This is the main class used to implement control of the player.
+    /// It is a superset of the AnimationController class, but is inlined to allow for any kind of customisation.
+    /// </summary>
+    public class PlayerController : KinematicObject
+    {
+        public AudioClip jumpAudio;
+        public AudioClip respawnAudio;
+        public AudioClip ouchAudio;
+
+        /// <summary>
+        /// Max horizontal speed of the player.
+        /// </summary>
+        public float maxSpeed = 7;
+        /// <summary>
+        /// Initial jump velocity at the start of a jump.
+        /// </summary>
+        public float jumpTakeOffSpeed = 7;
+
+        public JumpState jumpState = JumpState.Grounded;
+        private bool stopJump;
+        /*internal new*/
+        public Collider2D collider2d;
+        /*internal new*/
+        public AudioSource audioSource;
+        public Health health;
+        public bool controlEnabled = true;
+        public float maxDoubleJumpHeight = 2f;
+        private bool canDoubleJump = false;
+
+        bool jump;
+        Vector2 move;
+        SpriteRenderer spriteRenderer;
+        internal Animator animator;
+        readonly PlatformerModel model = Simulation.GetModel<PlatformerModel>();
+
+        // Declare variables para un dash
+        public float dashDuration = 0.5f; 
+        public float dashSpeed = 10f; 
+        private bool isDashing = false;
+
+        public Bounds Bounds => collider2d.bounds;
+
+        void Awake()
+        {
+            health = GetComponent<Health>();
+            audioSource = GetComponent<AudioSource>();
+            collider2d = GetComponent<Collider2D>();
+            spriteRenderer = GetComponent<SpriteRenderer>();
+            animator = GetComponent<Animator>();
+        }
+
+        protected override void Update()
+        {
+            if (controlEnabled)
+            {
+                //aqui aplicamos la mecanica para doble salto 
+                if (IsGrounded)
+                {
+                    canDoubleJump = true;
+                }
+
+                move.x = Input.GetAxis("Horizontal");
+                if (jumpState == JumpState.Grounded && Input.GetButtonDown("Jump"))
+                {
+                    jumpState = JumpState.PrepareToJump;
+
+                    //le agregue un if para que cuando haga un salto le permita hacer un segundo pero que se detenga cuando llegue a la altura maxima del salto doble
+                } else if (Input.GetButtonDown("Jump") && canDoubleJump)
+                {
+                    Jump(maxDoubleJumpHeight);
+                    canDoubleJump = false;
+                }
+                else if (Input.GetButtonUp("Jump"))
+                {
+                    stopJump = true;
+                    Schedule<PlayerStopJump>().player = this;
+                }//Aqui llame al shift para que funcione como un dash pero no el juego no lo reconoce
+                if (Input.GetKeyDown(KeyCode.LeftShift) && !isDashing)
+                {                 
+                    StartCoroutine(Dash());
+                }
+            }
+            else
+            {
+                move.x = 0;
+            }
+            UpdateJumpState();
+            base.Update();
+        }
+        private void Jump(float jumpHeight)
+        {
+            velocity.y = Mathf.Sqrt(1 * jumpHeight * Mathf.Abs(Physics2D.gravity.y));
+        }
+        private IEnumerator Dash()
+        {
+            isDashing = true;
+
+            Vector2 originalVelocity = velocity;
+
+            velocity = new Vector2(dashSpeed * transform.localScale.x, 0);
+
+            yield return new WaitForSeconds(dashDuration);
+
+            velocity = originalVelocity;
+
+            isDashing = false;
+        }
+
+        void UpdateJumpState()
+        {
+            jump = false;
+            switch (jumpState)
+            {
+                case JumpState.PrepareToJump:
+                    jumpState = JumpState.Jumping;
+                    jump = true;
+                    stopJump = false;
+                    break;
+                case JumpState.Jumping:
+                    if (!IsGrounded)
+                    {
+                        Schedule<PlayerJumped>().player = this;
+                        jumpState = JumpState.InFlight;
+                    }
+                    break;
+                case JumpState.InFlight:
+                    if (IsGrounded)
+                    {
+                        Schedule<PlayerLanded>().player = this;
+                        jumpState = JumpState.Landed;
+                    }
+                    break;
+                case JumpState.Landed:
+                    jumpState = JumpState.Grounded;
+                    break;
+            }
+        }
+
+        protected override void ComputeVelocity()
+        {
+            if (jump && IsGrounded)
+            {
+                velocity.y = jumpTakeOffSpeed * model.jumpModifier;
+                jump = false;
+            }
+            else if (stopJump)
+            {
+                stopJump = false;
+                if (velocity.y > 0)
+                {
+                    velocity.y = velocity.y * model.jumpDeceleration;
+                }
+            }
+
+            if (move.x > 0.01f)
+                spriteRenderer.flipX = false;
+            else if (move.x < -0.01f)
+                spriteRenderer.flipX = true;
+
+            animator.SetBool("grounded", IsGrounded);
+            animator.SetFloat("velocityX", Mathf.Abs(velocity.x) / maxSpeed);
+
+            targetVelocity = move * maxSpeed;
+        }
+
+        public enum JumpState
+        {
+            Grounded,
+            PrepareToJump,
+            Jumping,
+            InFlight,
+            Landed
+        }
+    }
+}
